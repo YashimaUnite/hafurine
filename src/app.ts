1a0eb02193b97995858f82e910c18a5eadbd2cd2
eienheiwa <eienheiwa@outlook.com>
2022-06-07 02:15:42 +0900

    Revert "Merge pull request #5 from eienheiwa/feature-DB"
    This reverts commit 8e5df88e981aa7e26a6329b3e7ab625b120427aa, reversing
    changes made to 337e8f0f2adceb4bf1d2ea7c538155822f8d40ea.

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,15 @@
 import express from 'express';
 import cors from 'cors';
 import { createRouter } from "./routes";
-import { AppDataSource } from "./data-source";
-AppDataSource.initialize().then(() => {
-  const app = express();
-  app.use(cors());
-  app.use(express.urlencoded({ extended: true }));
 
-  const port = 3000;
+const app = express();
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 
-  app.use('/', createRouter());
+const port = 3000;
 
-  app.listen(port, () => {
-    console.log(`Listening at http://localhost:${port}/`);
-  });
-}).catch((error: any) => {
-    console.log(`${error}`);
+app.use('/', createRouter());
+
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}/`);
 });
